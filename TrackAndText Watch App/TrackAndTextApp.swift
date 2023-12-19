c99860bceab8081bc264c4309ec9f4f108f12029
pritomtanvir <tanvirpritom@gmail.com>
2023-12-19 11:06:09 -0500

    Initial Commit.

---
+++ TrackAndText Watch App/TrackAndTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackAndTextApp.swift
+//  TrackAndText Watch App
+//
+//  Created by Tanvir Rahman on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackAndText_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
